26d06b1e1d5735d370cf248e321d8a0edcba0796
Kooshaba <kooshaba@gmail.com>
2015-06-14 15:50:35 -0700

    cursor positions working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+var firebase = new Firebase("https://skupaj.firebaseio.com/");
+
+var cursors = firebase.child('cursors');
+
+var cursor = cursors.push(
+	{
+		x: 1,
+		y: 1
+	}
+);
+
+$('body').mousemove(function(e) {
+	cursor.update(
+	{
+		x: e.pageX,
+		y: e.pageY
+	})
+});
